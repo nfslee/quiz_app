19f8910102530791e11ae1aefbf0154e2e35f286
Nicholas Lee <leenicholas05@gmail.com>
2024-01-28 13:39:46 -0500

    Start screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.white, Colors.blue
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight
-            ),
-          ),
-          child: const StartScreen()
-        ),
-      ),
-    ), 
-  );
+  runApp(const Quiz());
 }
