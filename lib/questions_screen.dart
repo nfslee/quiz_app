54895f26a98343419bb84e13d6136f4a07ba98d8
Nicholas Lee <leenicholas05@gmail.com>
2024-02-06 20:17:06 -0500

    Custom styled button

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget{
   const QuestionsScreen({super.key});
@@ -12,6 +13,21 @@ class QuestionsScreen extends StatefulWidget{
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context){
-    return const Text("QuestionsScreen");
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('The question...',
+            style: TextStyle(color: Colors.white)
+          ),
+          const SizedBox(height: 30),
+          AnswerButton('test', () {}),
+          AnswerButton('test', () {}),
+          AnswerButton('test', () {}),
+          AnswerButton('test', () {}),
+        ],
+      ),
+    );
   }
 }
